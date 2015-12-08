3d9db15cd2a986daa0b5425340ac49ab8e212241
Andrew <tnhuynh@wpi.edu>
2015-12-08 00:13:28 -0500

    Added test file for sonar

---
+++ Sonar_testing/Sonar_testing.ino
@@ -0,0 +1,20 @@
+
+#include <NewPing.h>
+
+const int triggerPin = 23; // digital pin 23
+const int echoPin = 22; // digital pin 22
+int d;
+
+NewPing sonar(triggerPin, echoPin, 200);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  d = sonar.ping();
+  Serial.print("Ping: ");
+  Serial.print(d/US_ROUNDTRIP_CM);
+  Serial.println("cm");
+  delay(100);
+}
